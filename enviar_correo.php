d1641dbde17c51cdb93700c23c2622b0a7b6fa95
Michell Pacheco Tacay <90942815+silverblade34@users.noreply.github.com>
2023-04-26 14:46:13 -0500

    correos listo

--- enviar_correo.php
+++ enviar_correo.php
@@ -17,8 +17,8 @@ try {
     $mail->isSMTP(); //Send using SMTP
     $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
     $mail->SMTPAuth = true; //Enable SMTP authentication
-    $mail->Username = 'mpacheco.tacay@gmail.com'; //SMTP username
-    $mail->Password = 'avmsnsjpyyfppuot'; //SMTP password
+    $mail->Username = 'opseliperu@gmail.com'; //SMTP username
+    $mail->Password = 'xavuvvecvrwagnfh'; //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587; //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
@@ -26,7 +26,8 @@ try {
 
     //Recipients
     // $mail->setFrom('reportes.petrothor@gmail.com', 'Reportes Petrothor');   //Add a recipient
-    $mail->addAddress('mpacheco.tacay@gmail.com'); //Add a recipient
+    $mail->addAddress('worihuela@opseli.com'); //Add a recipient
+    $mail->addAddress('kpacheco@opseli.com'); 
 
     // $mail->addReplyTo('reportes.petrothor@gmail.com', 'Reportes Petrothor');
 // Obtén los datos del formulario
